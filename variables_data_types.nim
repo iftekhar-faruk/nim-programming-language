8eb0a57e4f0330b1f5e7f9cf6965a5363b4281d3
iftekhar-faruk <faruk.mist08@gmail.com>
2023-11-11 11:54:40 +0600

    Second Commit

---
+++ variables_data_types.nim
@@ -0,0 +1,20 @@
+#[
+    "dsfds" -> string
+    float -> 9.3123 3.14
+    int -> 1 2 
+    uint -> 100
+    float64 -> 3.13131313
+    bool -> true (yes) false (no)
+    char -> 's'
+]#
+
+var age: int = 28
+
+var name = "Mike"
+
+echo "Hello my name is ", name, ". I am age years old."
+echo "I like my nmae, ", name, ", it is a cool name"
+echo "Next year I will be ", age+1, " years old, and I want to change my name to Nck"
+
+const x = 10
+echo x
